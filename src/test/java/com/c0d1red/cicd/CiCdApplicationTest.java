edf24c262bb2ec8c4884a11f415fdf44fba08c69
Roman <31551955+C0d1red@users.noreply.github.com>
2021-03-20 21:45:53 +0300

    Test project added

---
+++ src/test/java/com/c0d1red/cicd/CiCdApplicationTest.java
@@ -0,0 +1,12 @@
+package com.c0d1red.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTest {
+
+    @Test
+    void dummyTest() { }
+
+}
